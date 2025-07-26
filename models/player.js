1ca2caefc3d61213b7b4f8996f48998cd7c43ffb
naveen199201 <99502425+naveen199201@users.noreply.github.com>
2025-07-26 21:07:55 +0530

    Create player.js

---
+++ models/player.js
@@ -0,0 +1,8 @@
+import mongoose from 'mongoose';
+
+const playerSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  wallet: { type: Number, default: 0 }
+}, { timestamps: true });
+
+export default mongoose.model('Player', playerSchema);
